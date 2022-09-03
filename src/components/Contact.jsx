20ea175838a0c4abfeaec6ddab20df2468a9c456
Adriane James J. Puzon <adrianejames27@gmail.com>
2022-09-04 02:45:31 +0800

    animate contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,6 @@
-import Button from './Button'
-import { motion } from 'framer-motion'
 import AnimatedPage from './AnimatedPage'
 
-export default function Contact({onClick}) {
+export default function Contact() {
     return (
         <AnimatedPage>
             <div>
@@ -13,9 +11,6 @@ export default function Contact({onClick}) {
                     <i className="bi bi-envelope m-5"></i>
                 </a>
             </div>
-            <motion.div>
-                <Button text="Hide" onClick={onClick}/>
-            </motion.div>
         </AnimatedPage>    
     )
 }
